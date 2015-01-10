cdb80a202085e1647f1cb7daa9c313de59ea4d0c
Christian Fei <crifei93@gmail.com>
2015-01-10 20:10:21 +0100

    renamed user1ResourcePath

--- test/Resource.spec.js
+++ test/Resource.spec.js
@@ -5,7 +5,7 @@ describe('sample', function() {
 
   /* helpers */
   var userResourceUrl = '/users/:userId';
-  var user1ResourceRegExp = /\/users\/1\/?$/;
+  var user1ResourcePath = /\/users\/1\/?$/;
   var userAndBooksResourceUrl = '/users/:userId/books/:bookId';
   var user1ResourceUrl = '/users/1';
   var dummyUser = { name:'Elvis', lastName:'Presley' };
@@ -34,14 +34,14 @@ describe('sample', function() {
   });
 
   it('should get a single resource when passed in the id', function () {
-    $httpBackend.expectGET(user1ResourceRegExp).respond(200,dummyUser)
+    $httpBackend.expectGET(user1ResourcePath).respond(200,dummyUser)
     var user = new Resource( userResourceUrl, {} );    
     user.get({userId:1});
     $httpBackend.flush();
   });
 
   it('should get a single resource and not nested if only parameter for first resource is passed in', function () {
-    $httpBackend.expectGET(user1ResourceRegExp).respond(200,dummyUser)
+    $httpBackend.expectGET(user1ResourcePath).respond(200,dummyUser)
     var user = new Resource( userAndBooksResourceUrl, {} );    
     user.get({userId:1});
     $httpBackend.flush();
