9e78825d0719db09e0b8844106431448578a24a8
Christian Fei <crifei93@gmail.com>
2015-01-10 20:28:40 +0100

    added test for resource test and clean up

--- test/Resource.spec.js
+++ test/Resource.spec.js
@@ -7,6 +7,8 @@ describe('Resource', function() {
   var userResourceUrl = '/users/:userId';
   var user1ResourcePath = /\/users\/1\/?$/;
   var userAndBooksResourceUrl = '/users/:userId/books/:bookId';
+  var usersResourceUrl = '/users/';
+  var usersResourcePath = /\/users\/?$/;
   var user1ResourceUrl = '/users/1';
   var dummyUser = { name:'Elvis', lastName:'Presley' };
 
@@ -33,29 +35,36 @@ describe('Resource', function() {
     expect( user.getResourceUrl() ).to.equal( user1ResourceUrl );
   });
 
+  it('should get the resource set when no parameters are passed in and no defaultParameters are set', function () {
+    $httpBackend.expectGET(usersResourceUrl).respond(200,dummyUser)
+    var user = new Resource( usersResourceUrl, {} );    
+    user.get();
+    $httpBackend.flush();    
+  });
+
   it('should get a single resource when passed in the id', function () {
-    $httpBackend.expectGET(user1ResourcePath).respond(200,dummyUser)
+    $httpBackend.expectGET( user1ResourcePath ).respond(200,dummyUser)
     var user = new Resource( userResourceUrl, {} );    
     user.get({userId:1});
     $httpBackend.flush();
   });
 
   it('should get a single resource and not nested if only parameter for first resource is passed in', function () {
-    $httpBackend.expectGET(user1ResourcePath).respond(200,dummyUser)
+    $httpBackend.expectGET( user1ResourcePath ).respond(200,dummyUser)
     var user = new Resource( userAndBooksResourceUrl, {} );    
     user.get({userId:1});
     $httpBackend.flush();
   });
 
   it('should get the nested resource until the defined parameters', function () {
-    $httpBackend.expectGET(/\/users\/1\/books\/1\/?$/).respond(200,dummyUser)
+    $httpBackend.expectGET( /\/users\/1\/books\/1\/?$/ ).respond(200,dummyUser)
     var userBooks = new Resource( userAndBooksResourceUrl, {} );
     userBooks.get({userId:1,bookId:1});
     $httpBackend.flush();
   });
 
   it('should fallback to defaultParameters when parameters during operation are not passed in', function () {
-    $httpBackend.expectGET(user1ResourcePath).respond(200,dummyUser)
+    $httpBackend.expectGET( user1ResourcePath ).respond(200,dummyUser)
     var user = new Resource( userAndBooksResourceUrl, {userId:1} );    
     user.get();
     $httpBackend.flush();    
