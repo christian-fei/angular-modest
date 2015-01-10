a8e5f50c0d0cab1140c1e2792880e2468747c53e
Christian Fei <crifei93@gmail.com>
2015-01-10 13:40:42 +0100

    added test for getting a single resource

--- src/angular-modest.js
+++ src/angular-modest.js
@@ -2,7 +2,7 @@
   'use strict';
   var modest = angular.module('modest',[]);
 
-  modest.factory('Resource',[function(){
+  modest.factory('Resource',['$http',function($http){
     var Resource = function(url,paramDefaults,actions,nestedResources){
       var self = this;
       var _paramDefaults = paramDefaults||{};
@@ -10,6 +10,10 @@
 
       self.getUrl = function(){
         return _url;
+      };
+
+      self.get = function(parameters){
+        $http.get(parameterize(_url,parameters));
       };
 
       function parameterize(url,parameters){
--- test/sample.spec.js
+++ test/sample.spec.js
@@ -1,14 +1,17 @@
 describe('sample', function() {
   var Resource;
 
+  var $httpBackend;
+
   /* helpers */
-  var userResourceUrl = '/user/:userId';
-  var user1ResourceUrl = '/user/1';
+  var userResourceUrl = '/users/:userId';
+  var user1ResourceUrl = '/users/1';
 
   beforeEach(module('modest'));
 
-  beforeEach(inject(function(_Resource_){
+  beforeEach(inject(function(_Resource_,_$httpBackend_){
     Resource = _Resource_;
+    $httpBackend = _$httpBackend_;
   }));
   
 
@@ -23,5 +26,16 @@ describe('sample', function() {
   it('should parameterize the resourceUrl if default parameters have been passed in', function () {
     var resource = new Resource( userResourceUrl, {userId:1} );
     expect( resource.getUrl() ).to.equal( user1ResourceUrl );
+  });
+
+  it('should get a single resource when passed in the id', function () {
+    $httpBackend.expectGET(/\/users\/1/).respond(200,{
+      name:'Elvis',
+      lastName:'Presley'
+    })
+    var resource = new Resource( userResourceUrl, {} );    
+    resource.get({userId:1});
+
+    $httpBackend.flush();
   });
 });
