dce8c6423027c5eff2a17b94a79593083e430b34
Christian Fei <crifei93@gmail.com>
2015-01-11 14:05:23 +0100

    added POC test for multiple nested resource

--- test/Resource.spec.js
+++ test/Resource.spec.js
@@ -7,10 +7,11 @@ describe('Resource', function() {
   var userResourceUrl = '/users/:userId';
   var user1ResourceUrl = '/users/1';
   var user1ResourceMatch = /\/users\/1\/?$/;
-  var userAndBooksResourceUrl = '/users/:userId/books/:bookId';
-  var user1Book1ResourceMatch = /\/users\/1\/books\/1\/?$/;
   var usersResourceUrl = '/users/';
   var usersResourceMatch = /\/users\/?$/;
+  var userBooksResourceUrl = '/users/:userId/books/:bookId';
+  var user1Book1ResourceMatch = /\/users\/1\/books\/1\/?$/;
+  var userBookSomethingResourceUrl = '/users/:userId/books/:bookId/something/:somethingId';
   var dummyUser = { name:'Elvis', lastName:'Presley' };
   var dummyBook = { name:'Moby Dick', author:'Herman Melville' };
 
@@ -53,33 +54,33 @@ describe('Resource', function() {
 
   it('should get a single resource and not nested if only parameter for first resource is passed in', function () {
     $httpBackend.expectGET( user1ResourceMatch ).respond(200,dummyUser)
-    var user = new Resource( userAndBooksResourceUrl, {} );
+    var user = new Resource( userBooksResourceUrl, {} );
     user.get({userId:1});
     $httpBackend.flush();
   });
 
   it('should get the nested resource until the defined parameters', function () {
     $httpBackend.expectGET( user1Book1ResourceMatch ).respond(200,dummyBook)
-    var userBooks = new Resource( userAndBooksResourceUrl, {} );
+    var userBooks = new Resource( userBooksResourceUrl, {} );
     userBooks.get({userId:1,bookId:1});
     $httpBackend.flush();
   });
 
   it('should fallback to defaultParameters when parameters during operation are not passed in', function () {
     $httpBackend.expectGET( user1ResourceMatch ).respond(200,dummyUser)
-    var user = new Resource( userAndBooksResourceUrl, {userId:1} );
+    var user = new Resource( userBooksResourceUrl, {userId:1} );
     user.get();
     $httpBackend.flush();
   });
 
   describe('nested Resource', function () {
     it('should have a property of type Resource if unresolved nested resource is available', function () {
-      var user1 = new Resource( userAndBooksResourceUrl, {userId:1} );
+      var user1 = new Resource( userBooksResourceUrl, {userId:1} );
       expect(user1).to.have.property('books');
       expect(user1.books).to.be.an.instanceof(Resource);
     });
     it('should get a single nested resource when passed in the id', function () {
-      var user1 = new Resource( userAndBooksResourceUrl, {userId:1} );
+      var user1 = new Resource( userBooksResourceUrl, {userId:1} );
 
       $httpBackend.expectGET( user1Book1ResourceMatch ).respond(200,dummyBook)
       user1.get({bookId:1});
@@ -92,6 +93,12 @@ describe('Resource', function() {
       $httpBackend.expectGET( user1Book1ResourceMatch ).respond(200,dummyBook)
       user1.books.get(1);
       $httpBackend.flush();
+    });
+
+    it('should have properties for each nested resource', function () {
+      var user1Book1 = new Resource( userBookSomethingResourceUrl, {userId:1,bookId:1} );
+      expect(user1Book1).to.have.property('something');
+      expect(user1Book1.something).to.be.an.instanceof(Resource);
     });
   });
 
